5167b1b9742c6adaacbbc7a1eebc97403da25d49
Yu-Lun Tsai <stormysun513@hotmail.com>
2018-04-11 00:57:00 -0700

    rename instruction files

--- new_tab_on_mac_terminal.sh
+++ new_tab_on_mac_terminal.sh
@@ -4,11 +4,10 @@ function main() {
     osascript 2>/dev/null <<EOF
     tell application "System Events"
         tell process "Terminal" to keystroke "t" using command down
-    end 
+    end tell 
     tell application "Terminal"
         do script with command "clear" in window 1
         do script with command "cd \"$PWD\"; $*" in window 1
-        do script with command "echo hello" in window 1
     end tell
 EOF
 }
