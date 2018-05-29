cb43843f767afcbb42f417d09c5b5dc6496393e9
Yu-Lun Tsai <stormysun513@hotmail.com>
2018-05-29 10:43:56 -0700

    fix svn-diff-wrapper bug when revision is specified instead of diff with working copy

--- svndiffwrapper.sh
+++ svndiffwrapper.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+# it returns '(working' or '(revision'
+TAG=`echo "${5}" | awk '{print $2}'`
+
+# fetch the file path and open it if $TAG is '(working'
 WORKING_COPY=`echo "${5}" | awk '{print $1}'`
 
-# ${5} is the working copy while ${7} is a temp file
-/usr/bin/vimdiff ${6} $WORKING_COPY
+if [ "$TAG" = "(working" ];then
+    # ${5} is the working copy while ${7} is a temp file
+    /usr/bin/vimdiff ${6} $WORKING_COPY
+else
+    # open the readonly file if diffing an old revision
+    /usr/bin/vimdiff ${6} ${7}
+fi
